b927e42f0efcc32c80451c7d6a998d9190262828
heriet <admin@heriet.info>
2016-01-08 17:51:11 +0900

    refactor change copyright

--- hxpixel/bytes/BytesInputWrapper.hx
+++ hxpixel/bytes/BytesInputWrapper.hx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Heriet [http://heriet.info/].
+ * Copyright (c) heriet [http://heriet.info/].
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
